ccc25ca28a2d3e33fbac4cdce212e473b489ab8c
maurizioandreotti <maurizioandreottilc@gmail.com>
2020-04-07 16:29:34 +0200

    candidate as tested on raspi, and compiled and tested on win10 and xp

--- distXP.bat
+++ distXP.bat
@@ -1,7 +1,7 @@
 
 echo building distibution
 
-python setup.py py2exe > dist_0.3.5.log
+python setup.py py2exe > compile.log
 
 echo copying files
 
@@ -16,33 +16,41 @@ xcopy C:\Python27\Lib\site-packages\gtk-2.0\runtime\share\themes       dist\shar
 xcopy C:\Python27\Lib\site-packages\gtk-2.0\runtime\lib                dist\lib /s /d /y /f /i
 xcopy C:\Python27\lib\site-packages\gtk-2.0\runtime\bin\libxml2-2.dll  dist  /y
 
-xcopy C:\WINDOWS\system32\USP10.DLL    dist /y
-xcopy C:\WINDOWS\system32\OLEAUT32.dll  dist /y
-xcopy C:\WINDOWS\system32\USER32.dll    dist /y
-xcopy C:\WINDOWS\system32\IMM32.DLL     dist /y
-xcopy C:\WINDOWS\system32\MPR.dll       dist /y
-xcopy C:\WINDOWS\system32\SHLWAPI.DLL   dist /y
+
 xcopy C:\WINDOWS\system32\ADVAPI32.dll  dist /y
-xcopy C:\WINDOWS\system32\msvcrt.dll    dist /y
-xcopy C:\WINDOWS\system32\WS2_32.dll    dist /y
-xcopy C:\WINDOWS\system32\GDI32.dll     dist /y
-xcopy C:\WINDOWS\system32\gdiplus.dll   dist /y
-xcopy C:\WINDOWS\system32\NETAPI32.dll  dist /y
-xcopy C:\WINDOWS\system32\WINMM.dll     dist /y
-xcopy C:\WINDOWS\system32\CRYPT32.dll   dist /y
-xcopy C:\WINDOWS\system32\KERNEL32.dll  dist /y
-xcopy C:\WINDOWS\system32\MSIMG32.DLL   dist /y
-xcopy C:\WINDOWS\system32\MSWSOCK.dll   dist /y
-xcopy C:\WINDOWS\system32\WSOCK32.dll   dist /y
-xcopy C:\WINDOWS\system32\DNSAPI.DLL    dist /y
-xcopy C:\WINDOWS\system32\VERSION.dll   dist /y
-xcopy C:\WINDOWS\system32\OLE32.dll     dist /y
-xcopy C:\WINDOWS\system32\SHELL32.DLL   dist /y
 xcopy C:\WINDOWS\system32\COMDLG32.dll  dist /y
 xcopy C:\WINDOWS\system32\COMCTL32.dll  dist /y
+xcopy C:\WINDOWS\system32\CRYPT32.dll   dist /y
+
+xcopy C:\WINDOWS\system32\GDI32.dll     dist /y
+xcopy C:\WINDOWS\system32\gdiplus.dll   dist /y
+
+xcopy C:\WINDOWS\system32\KERNEL32.dll  dist /y
+
+xcopy C:\WINDOWS\system32\MPR.dll       dist /y
+xcopy C:\WINDOWS\system32\msvcrt.dll    dist /y
+xcopy C:\WINDOWS\system32\MSIMG32.DLL   dist /y
+xcopy C:\WINDOWS\system32\MSWSOCK.dll   dist /y
+
+
+xcopy C:\WINDOWS\system32\OLE32.dll     dist /y
+xcopy C:\WINDOWS\system32\OLEAUT32.dll  dist /y
+
+xcopy C:\WINDOWS\system32\SHELL32.DLL   dist /y
+xcopy C:\WINDOWS\system32\SHLWAPI.DLL   dist /y
+
+xcopy C:\WINDOWS\system32\USER32.dll    dist /y
+xcopy C:\WINDOWS\system32\USP10.DLL    dist /y
+xcopy C:\WINDOWS\system32\VERSION.dll   dist /y
+xcopy C:\WINDOWS\system32\WINMM.dll     dist /y
 xcopy C:\WINDOWS\system32\WINSPOOL.DRV  dist /y
 
 
+rem xcopy C:\WINDOWS\system32\DNSAPI.DLL    dist /y
+rem xcopy C:\WINDOWS\system32\IMM32.DLL     dist /y
+rem xcopy C:\WINDOWS\system32\NETAPI32.dll  dist /y
+rem xcopy C:\WINDOWS\system32\WS2_32.dll    dist /y
+rem xcopy C:\WINDOWS\system32\WSOCK32.dll   dist /y
 
 pause
 
