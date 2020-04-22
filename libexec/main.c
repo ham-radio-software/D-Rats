0b77a8b6fdb3a7d0948eb51b99eef8f5d76e9a95
Marius, YO2LOJ <marius@yo2loj.ro>
2020-04-22 20:05:14 +0300

    Added return error code compression/decompression error

--- libexec/main.c
+++ libexec/main.c
@@ -1,3 +1,29 @@
+/*
+ Copyright 2020 Marius Petrescu, YO2LOJ <marius@yo2loj.ro>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+    To compile the files under Windows use OpenWatcom C
+    You need to define LZHUF in your project setup
+
+    On Linux, just issue a 'make' command
+*/
+
+
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -7,6 +33,7 @@
 int main(int argc, char *argv[])
 {
         char  *s;
+        int result;
 
         struct lzhufstruct * huf = AllocStruct();
 
@@ -21,11 +48,13 @@ int main(int argc, char *argv[])
                 return 1;
         }
         if (toupper(*argv[1]) == 'E')
-                Encode(0, argv[2], argv[3], huf, 0);
+                result = Encode(0, argv[2], argv[3], huf, 0);
         else
-                Decode(0, argv[2], argv[3], huf, 0);
+                result = Decode(0, argv[2], argv[3], huf, 0);
 
         FreeStruct(huf);
 
-        return 0;
+        if (result != 0) result = 1;
+
+        return result;
 }
