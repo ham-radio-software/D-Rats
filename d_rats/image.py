f7e7c6588f120809f7aaf9a7baa700c96c2c5b63
John E. Malmberg <wb8tyw@gmail.com>
2021-02-20 14:32:17 -0600

    d_rats/image.py python3 compatible
    The python image library has changed for python 3.
    
    First pass at silencing pylint issues.

--- d_rats/image.py
+++ d_rats/image.py
@@ -1,15 +1,16 @@
 from __future__ import absolute_import
 from __future__ import print_function
-import gi
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
 
 import tempfile
 import os
 
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
+
 from . import inputdialog
 from . import miscwidgets
-from . import dplatform #imported by kater apparently not used...
+# from . import dplatform #imported by kater apparently not used...
 
 SIZES = [
     "160x120",
@@ -29,120 +30,103 @@ SIZES = [
     ]
 
 
-def has_image_support():
-    '''Has Image Support'''
-    global IMAGE
-    try:
-        from PIL import Image
-    except ImportError:
-        try:
-            import Image
-        except ImportError:
-            return False
-
-    IMAGE = Image
-
-    return True
-
-
 def update_image(filename, dlg):
     '''Update Image'''
     reqsize = dlg.size.get_active_text()
     if "x" in reqsize:
         _h, _w = reqsize.split("x")
 
-        h = int(_h)
-        w = int(_w)
+        height = int(_h)
+        width = int(_w)
     elif "%" in reqsize:
         factor = float(reqsize[0:2]) / 100.0
-        h, w = dlg.image.size
+        height, width = dlg.image.size
 
-        w = int(w * factor)
-        h = int(h * factor)
+        width = int(width * factor)
+        height = int(height * factor)
     else:
-        h, w = dlg.image.size
+        height, width = dlg.image.size
 
-    resized = dlg.image.resize((h, w))
+    resized = dlg.image.resize((height, width))
 
     (base, _ext) = os.path.splitext(os.path.basename(filename))
     dlg.resized = os.path.join(tempfile.gettempdir(),
                                "resized_" + base + ".jpg")
     resized.save(dlg.resized, quality=dlg.quality)
-    
+
     print("Saved to %s" % dlg.resized)
 
-    f = open(dlg.resized)
-    f.seek(0, 2)
-    size = f.tell()
-    f.close()
+    file_handle = open(dlg.resized)
+    file_handle.seek(0, 2)
+    size = file_handle.tell()
+    file_handle.close()
 
-    dlg.sizelabel.set_text("%i KB" % (size >> 10))        
+    dlg.sizelabel.set_text("%i KB" % (size >> 10))
     dlg.preview.set_from_file(dlg.resized)
 
 
-def set_quality(scale, event, value, dlg):
+def set_quality(_scale, _event, value, dlg):
     '''Set Quality'''
     dlg.quality = int(value)
     dlg.update()
 
 
-def build_image_dialog(filename, image, dlgParent=None):
+def build_image_dialog(filename, image, dialog_parent=None):
     '''Build Image Dialog'''
-    d = inputdialog.FieldDialog(title="Send Image",
-                                parent=dlgParent)
+    dialog = inputdialog.FieldDialog(title="Send Image",
+                                     parent=dialog_parent)
 
     def update():
-        update_image(filename, d)
+        update_image(filename, dialog)
 
-    d.add_field(_("Filename"), Gtk.Label.new(os.path.basename(filename)))
+    dialog.add_field(_("Filename"), Gtk.Label.new(os.path.basename(filename)))
 
-    d.sizelabel = Gtk.Label.new("--")
-    d.add_field(_("Size"), d.sizelabel)
+    dialog.sizelabel = Gtk.Label.new("--")
+    dialog.add_field(_("Size"), dialog.sizelabel)
 
-    d.size = miscwidgets.make_choice(SIZES, False, SIZES[1])
-    d.size.connect("changed", lambda x: update())
-    d.add_field(_("Resize to"), d.size)
+    dialog.size = miscwidgets.make_choice(SIZES, False, SIZES[1])
+    dialog.size.connect("changed", lambda x: update())
+    dialog.add_field(_("Resize to"), dialog.size)
 
     quality = Gtk.HScale(Gtk.Adjustment.new(50, 1, 100, 10, 10, 0))
     quality.connect("format-value",
-                    lambda s,v: "%i" % v)
-    quality.connect("change-value", set_quality, d)
-    d.add_field(_("Quality"), quality)
+                    lambda s, v: "%i" % v)
+    quality.connect("change-value", set_quality, dialog)
+    dialog.add_field(_("Quality"), quality)
 
-    d.preview = Gtk.Image()
-    d.preview.show()
-    sw = Gtk.ScrolledWindow()
-    sw.add_with_viewport(d.preview)
-    sw.set_size_request(320,320)
-    d.add_field(_("Preview"), sw, full=True)
+    dialog.preview = Gtk.Image()
+    dialog.preview.show()
+    scrollw = Gtk.ScrolledWindow()
+    scrollw.add_with_viewport(dialog.preview)
+    scrollw.set_size_request(320, 320)
+    dialog.add_field(_("Preview"), scrollw, full=True)
 
-    d.set_size_request(400, 450)
+    dialog.set_size_request(400, 450)
 
-    d.image = image
-    d.resized = None
-    d.quality = 50
+    dialog.image = image
+    dialog.resized = None
+    dialog.quality = 50
 
-    d.update = update
-    d.update()
+    dialog.update = update
+    dialog.update()
 
-    return d
+    return dialog
 
 
-def send_image(filename, dlgParent=None):
+def send_image(filename, dialog_parent=None):
     '''Send Image'''
-    if not has_image_support():
+    try:
+        from PIL import Image, UnidentifiedImageError
+    except ImportError:
         msg = _("No support for resizing images.  Send unaltered?")
         from .ui import main_common
-        if main_common.ask_for_confirmation(msg, dlgParent):
+        if main_common.ask_for_confirmation(msg, dialog_parent):
             return filename
-        else:
-            return None
-
+        return None
     try:
-        img = IMAGE.open(filename)
-    except IOError as err:
-        print("%s: %s" % (filename, err))
-        dialog = gtk.MessageDialog(buttons=gtk.BUTTONS_OK, parent=dlgParent)
+        img = Image.open(filename)
+    except UnidentifiedImageError:
+        dialog = Gtk.MessageDialog(buttons=Gtk.BUTTONS_OK, parent=dialog_parent)
         dialog.set_property("text", _("Unknown image type"))
         dialog.run()
         dialog.destroy()
@@ -150,15 +134,14 @@ def send_image(filename, dlgParent=None):
 
     if img.mode in ("RGBA", "P"):
         img = img.convert("RGB")
-    dialog = build_image_dialog(filename, img, dlgParent)
+    dialog = build_image_dialog(filename, img, dialog_parent)
     run_status = dialog.run()
     temp_file = dialog.resized
     dialog.destroy()
 
-    if run_status == gtk.RESPONSE_OK:
+    if run_status == Gtk.ResponseType.OK:
         return temp_file
-    else:
-        return None
+    return None
 
 
 def main():
@@ -172,20 +155,14 @@ def main():
                                fallback=True)
     lang.install()
 
-    supported = has_image_support()
-
-    if supported:
-        try:
-            temp_file = send_image(sys.argv[1])
-            if temp_file:
-                print("sent_image_name %s" % temp_file)
-            else:
-                print('send_image returned None!')
-        except IndexError:
-            print("Image filename required!")
-    else:
-        print('No image Support found!')
-
+    try:
+        temp_file = send_image(sys.argv[1])
+        if temp_file:
+            print("sent_image_name %s" % temp_file)
+        else:
+            print('send_image returned None!')
+    except IndexError:
+        print("Image filename required!")
 
 if __name__ == "__main__":
     main()
