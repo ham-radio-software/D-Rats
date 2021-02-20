72e02fe33e083c7e118535a5e1e62d96b146906b
John E. Malmberg <wb8tyw@gmail.com>
2021-02-20 11:46:13 -0600

    d-rats/cap.py Convert to lxml
    Convert from libxml2 to lxml.
    
    Silence some pylint warnings.

--- d_rats/cap.py
+++ d_rats/cap.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+'''cap'''
 #
 # Copyright 2008 Dan Smith <dsmith@danplanet.com>
 #
@@ -17,36 +18,44 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
-#importing printlog() wrapper
-from .debug import printlog
 
-import libxml2
-import six.moves.urllib.request, six.moves.urllib.parse, six.moves.urllib.error
+# import libxml2
+import six.moves.urllib.request
+import six.moves.urllib.parse
+import six.moves.urllib.error
 import tempfile
 import datetime
+from lxml import etree
 
+# importing printlog() wrapper
+from .debug import printlog
 try:
     from hashlib import md5
 except ImportError:
-    printlog("Cap","       : Installing hashlib replacement hack")
+    printlog("Cap", "       : Installing hashlib replacement hack")
     from .utils import ExternalHash as md5
 
+
 def ev_cmp_exp(ev1, ev2):
+    '''Event cmp exp'''
     if ev1.expires < ev2.expires:
         return -1
-    else:
-        return 1
+    return 1
+
 
 def ev_cmp_eff(ev1, ev2):
+    '''Event cmp eff'''
     if ev1.effective < ev2.effective:
         return -1
-    else:
-        return 1
+    return 1
 
 
 FMT = "%Y-%m-%dT%H:%M:%S"
 
-class CAPEvent(object):
+
+# pylint: disable=too-many-instance-attributes
+class CAPEvent():
+    '''CAP Event'''
     def __init__(self):
         self.category = None
         self.event = None
@@ -64,26 +73,25 @@ class CAPEvent(object):
 #
 #        self.__dict__[name] = val
 
-    def from_libxml_node(self, infonode):
-        child = infonode.children
-        while child:
-            content = child.getContent().strip()
+    def from_lxml_node(self, infonode):
+        '''From lxml node'''
+        for child in infonode.getchildren():
+            content = child.text.strip()
 
-            if child.name in ["effective", "expires"]:
+            if child.id in ["effective", "expires"]:
                 content = datetime.datetime.strptime(content,
                                                      "%Y-%m-%dT%H:%M:%S")
 
-            if child.name in list(self.__dict__.keys()):
+            if child.id in list(self.__dict__.keys()):
                 self.__dict__[child.name] = content
 
-            child = child.next
-
     def __str__(self):
-        return "%s (%s): %s..." % (self.headline,
-                                   self.expires.strftime(FMT),
-                                   self.description[:20])
+        return "%s (%s): %.20s..." % (self.headline,
+                                      self.expires.strftime(FMT),
+                                      self.description)
 
     def report(self):
+        '''report'''
         return """
 %s (%s - %s)
 %s
@@ -92,9 +100,12 @@ class CAPEvent(object):
        self.expires.strftime(FMT),
        self.description)
 
-class CAPParser(object):
+
+class CAPParser():
+    '''CAP Parser'''
+
     def __init__(self, filename):
-        doc = libxml2.parseFile(filename)
+        doc = etree.parse(filename)
 
         root = doc.children
 
@@ -106,40 +117,51 @@ class CAPParser(object):
         while child:
             if child.name == "info":
                 try:
-                    ev = CAPEvent()
-                    ev.from_libxml_node(child)
+                    event = CAPEvent()
+                    event.from_lxml_node(child)
 
-                    hash = md5()
-                    hash.update(ev.description)
+                    md5_hash = md5()
+                    md5_hash.update(event.description)
 
-                    if hash.digest() not in hashes:
-                        self.events.append(ev)
-                        hashes.append(hash.digest())
+                    if md5_hash.digest() not in hashes:
+                        self.events.append(event)
+                        hashes.append(md5_hash.digest())
 
-                except Exception as e:
-                    printlog(("Unable to parse CAP node: %s (%s)" % (child.name, e)))
+                # pylint: disable=broad-except
+                except Exception as err:
+                    printlog("Unable to parse CAP node: %s (%s)" %
+                             (child.name, err))
 
             child = child.next
 
         self.events.sort(ev_cmp_eff)
 
     def expired_events(self):
-        return sorted([x for x in self.events if x.expires < datetime.datetime.now()],
+        '''Expired Events'''
+        return sorted([x for x in self.events
+                       if x.expires < datetime.datetime.now()],
                       ev_cmp_eff)
 
     def unexpired_events(self):
-        return sorted([x for x in self.events if x.expires > datetime.datetime.now()],
+        '''Unexpired Events'''
+        return sorted([x for x in self.events
+                       if x.expires > datetime.datetime.now()],
                       ev_cmp_eff)
 
     def events_expiring_after(self, date):
+        '''Events Expiring After'''
         return sorted([x for x in self.events if x.expires > date],
                       ev_cmp_eff)
 
     def events_effective_after(self, date):
+        '''Events Effective After'''
         return sorted([x for x in self.events if x.effective > date],
                       ev_cmp_eff)
 
+
 class CAPParserURL(CAPParser):
+    '''CAP Parse URL'''
+
     def __init__(self, url):
         tmpf = tempfile.NamedTemporaryFile()
         name = tmpf.name
@@ -149,18 +171,21 @@ class CAPParserURL(CAPParser):
 
         CAPParser.__init__(self, name)
 
-if __name__ == "__main__":
-    import sys
+
+def main():
+    '''Unit Test'''
 
     #cp = CAPParser(sys.argv[1])
-    cp = CAPParserURL("http://www.weather.gov/alerts/fl.cap")
+    capp = CAPParserURL("http://www.weather.gov/alerts/fl.cap")
 
     epoch = datetime.datetime(2008, 9, 29, 00, 59, 00)
 
-    c = 0
-    for i in cp.events_expiring_after(epoch):
+    count = 0
+    for i in capp.events_expiring_after(epoch):
         printlog((i.report()))
-        c += 1
+        count += 1
 
-    printlog(("%i events" % c))
-        
+    printlog(("%i events" % count))
+
+if __name__ == "__main__":
+    main()
