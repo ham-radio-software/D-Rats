ca1ae705d15f737b3ea8e6618f61bfe942e980cb
John E. Malmberg <wb8tyw@gmail.com>
2021-08-15 18:42:43 -0500

    Fix file and event log tabs
    mainwindow.glade:
      Fix formatting for file transfer tab
      Fix missing items for slection box for event log tab
    
    main_files.py:
      Fix file size generation code
      Fix selection of stations and port for a file transfer
      Fix GdkPixbuf conversion
      Add gettext import to make pylance happy
    
    main_event.py:
    station_status.py:
      Add gettext import to make pylance happy
    
    utils.py:
      pylance message suppression for import of six modules
      Note about modify_text method being deprecated.

--- d_rats/station_status.py
+++ d_rats/station_status.py
@@ -16,6 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import gettext
+
+_ = gettext.gettext
 STATUS_MAX = 9
 STATUS_MIN = 0
 
