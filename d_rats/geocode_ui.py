547a3ca96ee032bb1774051120a234262e9f8d5a
maurizioandreotti <maurizioandreottilc@gmail.com>
2021-10-08 06:16:34 +0200

    some fix to compile this beta on Ms-Wwin

--- d_rats/geocode_ui.py
+++ d_rats/geocode_ui.py
@@ -21,14 +21,21 @@ from __future__ import print_function
 import gtk
 import gobject
 from urllib2 import URLError
-try:
-    # pylint: disable=import-error
-    from geopy import geocoders
-except ImportError:
-    # WB8TYW: This will probably go away in the future.
-    # pylint: disable=import-error
-    from .geopy import geocoders
-    print("Falling back to built-in geocoders.")
+
+
+
+
+# try:
+    # # pylint: disable=import-error
+    # from geopy import geocoders
+# except ImportError:
+    # # WB8TYW: This will probably go away in the future.
+    # # pylint: disable=import-error
+    # from .geopy import geocoders
+    # print("Falling back to built-in geocoders.")
+from .geopy import geocoders
+
+
 
 #importing printlog() wrapper
 from .debug import printlog
