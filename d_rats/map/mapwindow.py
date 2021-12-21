7e48e12d2b9dd23fb4ec6ba792c78effc7228709
John E. Malmberg <wb8tyw@gmail.com>
2021-12-20 18:24:52 -0600

    Add marker popup menu
    d_rats/map/__init__.py:
      Add MarkerEditDialog and MarkerPopupModel.
    
    d_rats/map/mapbottompanel.py:
      marker_list is a TreeWidget in a Gtk.ScrolledWindow,
      not a Gtk.ScrolledWindow.
    
    d_rats/map/mapdraw.py:
      Add some code for debugging map_markers
    
    d_rats/map/mapmarkerlist.py:
      Convert to be a class based on a TreeWidget.
    
    d_rats/map/mapmenumodel.py:
      Fix a Docstring
    
    d_rats/map/mappopupmodel.py:
      Improve some Docstrings
      cleanup code
    
    d_rats/map/mapwidget.py:
      Add point_is_visable method.
    
    d_rats/map/mapwindow.py:
      Start implementing map markers.
    
    d_rats/map/markereditdialog.py:
      New module for the Marker Edit Dialog.
    
    d_rats/map/markerpopupmodel.py:
      New module for the marker popup model.

--- d_rats/map/mapwindow.py
+++ d_rats/map/mapwindow.py
@@ -36,8 +36,14 @@ from gi.repository import GLib
 from gi.repository import GObject
 
 from .. import dplatform
+from .. import inputdialog
+from .. import map_sources
+from .. import miscwidgets
+from .. import signals
 from .. import map_source_editor
+from ..gps import GPSPosition
 from .. import map as Map
+from ..ui.main_common import ask_for_confirmation
 
 # This makes pylance happy with out overriding settings
 # from the invoker of the class
@@ -62,15 +68,14 @@ class MapWindow(Gtk.ApplicationWindow):
 
     __gsignals__ = {
         "reload-sources" : (GObject.SignalFlags.RUN_LAST,
-                            GObject.TYPE_NONE, ())
-    }
-    #    "user-send-chat" : signals.USER_SEND_CHAT,
-    #    "get-station-list" : signals.GET_STATION_LIST,
-    #    }
+                            GObject.TYPE_NONE, ()),
+        "user-send-chat" : signals.USER_SEND_CHAT,
+        "get-station-list" : signals.GET_STATION_LIST
+        }
 
-    #_signals = {"user-send-chat" : None,
-    #            "get-station-list" : None,
-    #            }
+    _signals = {"user-send-chat" : None,
+                "get-station-list" : None
+                }
 
     STATUS_COORD = 0
     STATUS_CENTER = 1
@@ -83,15 +88,15 @@ class MapWindow(Gtk.ApplicationWindow):
 
         # self.connect("destroy", Gtk.main_quit)
         self.config = config
-        # self.marker_list = None
         # self.map_tiles = []
         self.logger.info("Testing MapWindow")
 
         self.center_mark = None
         self.tracking_enabled = False
         self.__last_motion = None
-        self.__temp_frac = 0.0
+        self._newcenter = None
         self.map_sources = []
+        self.points_visible = []
         # this parameter defines the dimension of the map behind the window
         # tiles SHALL be
         #  - ODD due to the mechanism used then to calculate the
@@ -129,6 +134,7 @@ class MapWindow(Gtk.ApplicationWindow):
         self.statusbox = Map.StatusBox()
         bottom_panel = Map.BottomPanel(self)
         self.mapcontrols = bottom_panel.controls
+        self.marker_list = bottom_panel.marker_list
         box.pack_start(self.scrollw, True, True, True)
         box.pack_start(bottom_panel, False, False, False)
         box.pack_start(self.statusbox, False, False, False)
@@ -139,6 +145,10 @@ class MapWindow(Gtk.ApplicationWindow):
 
         self.add(box)
         Map.PopupModel.add_actions(self)
+        self.marker_model = Map.MarkerPopupModel()
+        self.marker_model.add_actions(self)
+        self.marker_menu = Gtk.Menu.new_from_model(self.marker_model)
+        self.marker_menu.attach_to_widget(self)
 
     def add_map_source(self, maps):
         '''
@@ -148,6 +158,46 @@ class MapWindow(Gtk.ApplicationWindow):
         :type maps: list
         '''
         print("add_map_source %s" % maps, type(maps), type(self))
+
+    def add_point(self, source, point):
+        '''
+        Add Point.
+
+        :param source: Map source
+        :param point: Point to add
+        '''
+        # (_lat, _lon) = self.map.get_center()
+        center = GPSPosition(self.map_widget.position.latitude,
+                             self.map_widget.position.longitude)
+        this = GPSPosition(point.get_latitude(), point.get_longitude())
+
+        self.marker_list.add_item(source.get_name(),
+                                  point.get_visible(), point.get_name(),
+                                  point.get_latitude(),
+                                  point.get_longitude(),
+                                  center.distance_from(this),
+                                  center.bearing_to(this))
+        self.add_point_visible(point)
+        print("mapdisplay.MapWindow.add_point")
+        self.map_widget.queue_draw()
+
+    def add_point_visible(self, point):
+        '''
+        Add Point Visible.
+
+        :param point: Point to add
+        :returns: True if point is visible
+        '''
+        if point in self.points_visible:
+            self.points_visible.remove(point)
+
+        if self.map_widget.point_is_visible(point.get_latitude(),
+                                            point.get_longitude()):
+            if point.get_visible():
+                self.points_visible.append(point)
+                return True
+            return False
+        return False
 
     # Inherited from Parent and called by mainapp
     # def connect(self, signal name, function):
@@ -276,6 +326,104 @@ class MapWindow(Gtk.ApplicationWindow):
         '''
         self.save_map(self.get_visible_bounds())
 
+    def make_marker_popup(self, widget, view, event):
+        '''
+        Make Marker Popup.
+
+        :param widget: Widget with marker data
+        :type widget: :class:`Map.MapMarkerList`
+        :param view: View for popup
+        :type view: :class:`Gtk.Treeview`
+        :param event: Mouse click event
+        :type event: :class:`Gdk.Event`
+        '''
+        if event.button.button != 3:
+            return
+
+        if event.window == view.get_bin_window():
+            x_axis, y_axis = event.get_coords()
+            pathinfo = view.get_path_at_pos(int(x_axis), int(y_axis))
+            if pathinfo is None:
+                print("pathinfo is none")
+                return
+            view.set_cursor_on_cell(pathinfo[0], None, None, False)
+
+        (store, iter_value) = view.get_selection().get_selected()
+        try:
+            ident, = store.get(iter_value, 1)
+            group, = store.get(store.iter_parent(iter_value), 1)
+            print("ident", type(ident))
+            print("group", type(group))
+        except TypeError:
+            #ident = group = ""
+            ident="i"
+            group="g"
+            print("TypeError Seen")
+
+        self.marker_model.change_state(group, ident)
+        self.marker_menu.popup_at_widget(widget, Gdk.Gravity.STATIC,
+                                         Gdk.Gravity.STATIC, None)
+
+    def marker_center_handler(self, action, _value):
+        '''
+        Marker Center Handler.
+
+        :param action: Action that was invoked
+        :type action: :class:`GioSimpleAction`
+        :param _value: Value for action, Unused
+        :type _value: :class:`Gio.VariantType`
+        '''
+        print("self", type(self))
+        state = action.get_state()
+        group_var = state.get_child_value(0)
+        ident_var = state.get_child_value(1)
+        print("group_var", type(group_var))
+        print("ident_var", type(ident_var))
+        group = group_var.get_string()
+        ident = ident_var.get_string()
+        print("group", group)
+        print("ident", ident)
+
+    def marker_delete_handler(self, action, _value):
+        '''
+        Marker Delete Handler.
+
+        :param action: Action that was invoked
+        :type action: :class:`GioSimpleAction`
+        :param _value: Value for action, Unused
+        :type _value: :class:`Gio.VariantType`
+        '''
+        print("self", type(self))
+        state = action.get_state()
+        group_var = state.get_child_value(0)
+        ident_var = state.get_child_value(1)
+        print("group_var", type(group_var))
+        print("ident_var", type(ident_var))
+        group = group_var.get_string()
+        ident = ident_var.get_string()
+        print("group", group)
+        print("ident", ident)
+
+    def marker_edit_handler(self, action, _value):
+        '''
+        Marker Edit Handler.
+
+        :param action: Action that was invoked
+        :type action: :class:`GioSimpleAction`
+        :param value: Value for action, Unused
+        :type value: :class:`Gio.VariantType`
+        '''
+        print("self", type(self))
+        state = action.get_state()
+        group_var = state.get_child_value(0)
+        ident_var = state.get_child_value(1)
+        print("group_var", type(group_var))
+        print("ident_var", type(ident_var))
+        group = group_var.get_string()
+        ident = ident_var.get_string()
+        print("group", group)
+        print("ident", ident)
+
     def _make_menu(self):
         '''
         Menu for map.
@@ -295,12 +443,11 @@ class MapWindow(Gtk.ApplicationWindow):
         :param widget: Widget clicked on
         :type widget: :class:`Gtk.ScrolledWindow`
         :param event: Event that triggered this handler
-        :type event: :class:`Gtk.EventMotion`
+        :type event: :class:`Gdk.EventButton`
         :returns: True to stop other handlers from processing the event.
         :rtype: bool
         '''
         print("mouse_click_event")
-        print("widget", type("widget"), widget)
         x_axis, y_axis = event.get_coords()
 
         hadj = widget.get_hadjustment()
@@ -317,6 +464,7 @@ class MapWindow(Gtk.ApplicationWindow):
         # This is not a protected-access, it is the actual
         # python name for the type.
         # pylint: disable=protected-access
+        self._newcenter = position
         if event.button == 1 and event.type == Gdk.EventType._2BUTTON_PRESS:
             self.logger.info("recenter on %.s", position)
             self.recenter(position)
@@ -329,8 +477,6 @@ class MapWindow(Gtk.ApplicationWindow):
             popup_model = Map.PopupModel(position)
             popup_menu = Gtk.Menu.new_from_model(popup_model)
             popup_menu.attach_to_widget(self)
-            # Map.PopupModel.add_actions(self)
-            # self.grab_focus()
             popup_menu.popup_at_pointer()
         # elif event.type == Gdk.EventType.BUTTON_PRESS:
             #self.logger.info("Clicked: %s", position)
@@ -434,9 +580,11 @@ class MapWindow(Gtk.ApplicationWindow):
         :type _action: :class:`GioSimpleAction`
         :param _value: Value for action, Unused
         '''
-        print("self", type(self))
-        print("action", type(_action))
-        print("value", type(_value))
+        if self._newcenter:
+            self.recenter(self._newcenter)
+            self._newcenter = None
+            return
+        self.logger.info("popup center handler - no new position set")
 
     def popup_newmarker_handler(self, _action, _value):
         '''
@@ -449,6 +597,7 @@ class MapWindow(Gtk.ApplicationWindow):
         print("self", type(self))
         print("action", type(_action))
         print("value", type(_value))
+        self.set_mark_at()
 
     def popup_broadcast_handler(self, _action, _value):
         '''
@@ -461,6 +610,7 @@ class MapWindow(Gtk.ApplicationWindow):
         print("self", type(self))
         print("action", type(_action))
         print("value", type(_value))
+        self.prompt_to_send_loc()
 
     def printable_map(self, bounds=None):
         '''
@@ -515,6 +665,98 @@ class MapWindow(Gtk.ApplicationWindow):
 
     # Called by mainapp not sure if inherited
     # def queue_draw(self):
+
+    def prompt_to_send_loc(self):
+        '''
+        Prompt to send location.
+        '''
+        dialog = inputdialog.FieldDialog(title=_("Broadcast Location"))
+
+        callsign_e = Gtk.Entry()
+        callsign_e.set_max_length(8)
+        dialog.add_field(_("Callsign"), callsign_e)
+        desc_e = Gtk.Entry()
+        desc_e.set_max_length(20)
+        dialog.add_field(_("Description"), desc_e)
+        dialog.add_field(_("Latitude"), miscwidgets.LatLonEntry())
+        dialog.add_field(_("Longitude"), miscwidgets.LatLonEntry())
+        if self._newcenter:
+            lat = self._newcenter.latitude
+            lon = self._newcenter.longitude
+            dialog.get_field(_("Latitude")).set_text("%.4f" % lat)
+            dialog.get_field(_("Longitude")).set_text("%.4f" % lon)
+
+        while dialog.run() == Gtk.ResponseType.OK:
+            # pylint: disable=broad-except
+            try:
+                call = dialog.get_field(_("Callsign")).get_text()
+                desc = dialog.get_field(_("Description")).get_text()
+                lat = dialog.get_field(_("Latitude")).get_text()
+                lon = dialog.get_field(_("Longitude")).get_text()
+
+                fix = GPSPosition(lat=lat, lon=lon, station=call)
+                fix.comment = desc
+
+                for port in self.emit("get-station-list").keys():
+                    self.emit("user-send-chat",
+                              "CQCQCQ", port,
+                              fix.to_NMEA_GGA(), True)
+
+                break
+            except AttributeError:
+                # This happens when unit testing because mainapp has not
+                # been setup so nothing is returned for get-station-list
+                self.logger.info('prompt_to_send_log: failed to send',
+                                 exc_info=True)
+            except Exception as err:
+                # Eventually this handler should be removed.
+                print("Mapdiplay.MapWindow.prompt_to_send_loc",
+                      " Broad Exception (%s) %s" % (type(err), err))
+                # utils.log_exception()
+                except_dialog = Gtk.MessageDialog(buttons=Gtk.ButtonsType.OK,
+                                                  parent=dialog)
+                except_dialog.set_property("text",
+                                           _("Invalid value") + ": %s" % err)
+                except_dialog.run()
+                except_dialog.destroy()
+
+        dialog.destroy()
+
+    def prompt_to_set_marker(self, point, group=None):
+        '''
+        Prompt to set marker.
+
+        :param point: Point to set marker on
+        :param group: Optional group
+        :returns: Tuple of (point, group) or (None, None)
+        '''
+        # def do_address(_button, latw, lonw, namew):
+        #    dlg = geocode_ui.AddressAssistant()
+        #    run_status = dlg.run()
+        #    if run_status == Gtk.ResponseType.OK:
+        #        if not namew.get_text():
+        #            namew.set_text(dlg.place)
+        #        latw.set_text("%.5f" % dlg.lat)
+        #        lonw.set_text("%.5f" % dlg.lon)
+
+        dialog = Map.MarkerEditDialog()
+
+        sources = []
+        for src in self.map_sources:
+            if src.get_mutable():
+                sources.append(src.get_name())
+
+        dialog.set_groups(sources, group)
+        dialog.set_point(point)
+        run_status = dialog.run()
+        if run_status == Gtk.ResponseType.OK:
+            point = dialog.get_point()
+            group = dialog.get_group()
+        dialog.destroy()
+
+        if run_status == Gtk.ResponseType.OK:
+            return point, group
+        return None, None
 
     def recenter(self, position):
         '''
@@ -605,6 +847,47 @@ class MapWindow(Gtk.ApplicationWindow):
         '''
         Map.Tile.set_connected(connected)
 
+    def set_mark_at(self):
+        '''
+        Set Mark at.
+        '''
+        if not self._newcenter:
+            self.logger.info("Set Mark at - no new position set")
+            return
+
+        pos = map_sources.MapStation("STATION",
+                                     self._newcenter.latitude,
+                                     self._newcenter.longitude)
+        self._newcenter = None
+        pos.set_icon_from_aprs_sym("\\<")
+        point, group = self.prompt_to_set_marker(pos)
+        if not point:
+            return
+
+        for source in self.map_sources:
+            self.logger.info("set_mark_at: %s,%s",
+                             source.get_name(), group)
+            if source.get_name() == group:
+                self.logger.info("set_mark_at: Adding new point %s to %s",
+                                 point.get_name(),
+                                 source.get_name())
+                source.add_point(point)
+                source.save()
+                return
+        # No matching group
+        query = "%s %s %s" % (_("Group"), group,
+                              _("does not exist.  Do you want to create it?"))
+        if not ask_for_confirmation(query):
+            return
+
+        # pylint: disable=not-callable
+        src = map_sources.MapFileSource.open_source_by_name(self.config,
+                                                            group,
+                                                            True)
+        src.add_point(point)
+        src.save()
+        self.add_map_source(src)
+
     # Called by mainapp
     @staticmethod
     def set_proxy(proxy):
@@ -651,33 +934,33 @@ class MapWindow(Gtk.ApplicationWindow):
         print("self:", type(self))
         print("group:", type(group))
         print("vals:", type(vals))
-        #if group:
-        #    station = vals[1]
-        #else:
-        #    group = vals[1]
-        #    station = None
+        if group:
+            station = vals[1]
+        else:
+            group = vals[1]
+            station = None
 
-        # for src in self.map_sources:
-        #    if group != src.get_name():
-        #        continue
+        for src in self.map_sources:
+            if group != src.get_name():
+                continue
 
-        #    if station:
-        #        try:
-        #            point = src.get_point_by_name(station)
-        #        except KeyError:
-        #            continue
+            if station:
+                try:
+                    point = src.get_point_by_name(station)
+                except KeyError:
+                    continue
 
-        #        point.set_visible(vals[0])
-        #        self.add_point_visible(point)
-        #    else:
-        #        src.set_visible(vals[0])
-        #        for point in src.get_points():
-        #            point.set_visible(vals[0])
-        #            self.update_point(src, point)
+                point.set_visible(vals[0])
+                self.add_point_visible(point)
+            else:
+                src.set_visible(vals[0])
+                for point in src.get_points():
+                    point.set_visible(vals[0])
+                    self.update_point(src, point)
 
-        #    src.save()
-        #    break
-        # self.map_widget.queue_draw()
+            src.save()
+            break
+        self.map_widget.queue_draw()
 
     # Called from Mainapp
     def update_gps_status(self, gps_status):
@@ -690,3 +973,39 @@ class MapWindow(Gtk.ApplicationWindow):
 
         self.statusbox.sb_gps.pop(self.STATUS_GPS)
         self.statusbox.sb_gps.push(self.STATUS_GPS, gps_status)
+
+    def update_point(self, source, point):
+        '''
+        Update Point.
+
+        :param source: Map source
+        :param point: Point to update
+        '''
+        print("source", type(source))
+        print("point", type(point))
+        # = self.map_widget.position
+        center = GPSPosition(self.map_widget.position.latitude,
+                             self.map_widget.position.longitude)
+        this = GPSPosition(point.get_latitude(), point.get_longitude())
+
+        try:
+            self.marker_list.set_item(source.get_name(),
+                                      point.get_visible(),
+                                      point.get_name(),
+                                      point.get_latitude(),
+                                      point.get_longitude(),
+                                      center.distance_from(this),
+                                      center.bearing_to(this))
+        # pylint: disable=broad-except
+        except Exception as err:
+            print('mapdisplay.MapWindow.update_point: ',
+                  " (%s) %s" % (type(err), err))
+            if str(err) == "Item not found":
+                # this is evil
+                self.logger.info("Adding point instead of updating")
+                self.add_point(source, point)
+
+        self.add_point_visible(point)
+        print("mapdisplay.MapWindow.update_point source=%s point=%s",
+              source, point)
+        self.map_widget.queue_draw()
---
+++ d_rats/map/markerpopupmodel.py
@@ -0,0 +1,101 @@
+'''Map Marker Popup Menu Model Module.'''
+#
+# Copyright 2021 John Malmberg <wb8tyw@gmail.com>
+# Portions derived from works:
+# Copyright 2009 Dan Smith <dsmith@danplanet.com>
+# review 2019 Maurizio Andreotti  <iz2lxi@yahoo.it>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+# import logging
+
+import gi
+gi.require_version("Gtk", "3.0")
+
+from gi.repository import Gio
+from gi.repository import GLib
+
+
+# This makes pylance happy with out overriding settings
+# from the invoker of the class
+if not '_' in locals():
+    import gettext
+    _ = gettext.gettext
+
+
+class MarkerPopupModel(Gio.Menu):
+    '''
+    Creates the Popup Menu for Markers.
+    '''
+
+    def __init__(self):
+        Gio.Menu.__init__(self)
+
+        self.append(_("Edit"), 'win.edit_marker')
+        self.append(_("Delete"), 'win.delete_marker')
+        self.append(_("Center on this"), 'win.center_marker')
+        group_var = GLib.Variant.new_string("")
+        ident_var = GLib.Variant.new_string("")
+        params = GLib.Variant.new_tuple(group_var, ident_var)
+        self.action_center = Gio.SimpleAction.new_stateful(name='center_marker',
+                                                           parameter_type=None,
+                                                           state=params)
+        self.action_delete = Gio.SimpleAction.new_stateful(name='delete_marker',
+                                                           parameter_type=None,
+                                                           state=params)
+        self.action_edit = Gio.SimpleAction.new_stateful(name='edit_marker',
+                                                         parameter_type=None,
+                                                         state=params)
+
+    def add_actions(self, window):
+        '''
+        Add menu actions to the window.
+
+        :param window: The map window
+        :type window: :class:`Map.MapWindow`
+        '''
+        print("setup menu")
+        params_type = GLib.VariantType.new('r')
+        print("params_type", type(params_type))
+
+        self.action_center.connect('activate', window.marker_center_handler)
+        window.add_action(self.action_center)
+
+        self.action_delete.connect('activate', window.marker_delete_handler)
+        window.add_action(self.action_delete)
+
+        self.action_edit.connect('activate', window.marker_edit_handler)
+        window.add_action(self.action_edit)
+
+    def change_state(self, group, ident):
+        '''
+        Update state to current selection.
+
+        :param group: Group name for marker
+        :type group: str
+        :param ident: Identity of marker
+        :type ident: str
+        '''
+        group_var = GLib.Variant.new_string(group)
+        print("group_var", type(group_var))
+        ident_var = GLib.Variant.new_string(ident)
+        print("ident_var", type(ident_var))
+        params = GLib.Variant.new_tuple(group_var, ident_var)
+        self.action_center.change_state(params)
+        self.action_delete.change_state(params)
+        self.action_edit.change_state(params)
