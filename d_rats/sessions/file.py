f7cf75853ea0112316ea5ab53b77c955c4ad1c18
John E. Malmberg <wb8tyw@qsl.net>
2021-12-29 12:54:56 -0600

    Minor sessions file/form fixes (#24)
    d_rats/sessions/file.py:
      Add standard copyright message.
    
    d_rats/sessions/form.py:
      Add standard copyright message.
      Convert to use logging module.
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

--- d_rats/sessions/file.py
+++ d_rats/sessions/file.py
@@ -1,4 +1,20 @@
 '''Sessions file.py'''
+#
+# Copyright 2009 Dan Smith <dsmith@danplanet.com>
+# Python3 update Copyright 2021 John Malmberg <wb8tyw@qsl.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
 from __future__ import print_function
--- d_rats/sessions/form.py
+++ d_rats/sessions/form.py
@@ -1,11 +1,32 @@
 '''Form.'''
+#
+# Copyright 2009 Dan Smith <dsmith@danplanet.com>
+# Python3 update Copyright 2021 John Malmberg <wb8tyw@qsl.net>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import absolute_import
+
+import logging
 from d_rats.sessions import base
 from d_rats.sessions import file as sessions_file
-
 
 class FormTransferSession(sessions_file.FileTransferSession):
     '''Form Transfer Session.'''
 
     type = base.T_FORMXFER
+
+    def __init__(self, name, status_cb=None, **kwargs):
+        sessions_file.FileTransferSession.__init__(self, name, **kwargs)
+        self.logger = logging.getLogger("FormTransferSession")
