72859aa458d2dace55c6f9fbbde4cfd4fba1bcf5
MAURIZIO ANDREOTTI <maurizioandreottilc@gmail.com>
2022-02-01 15:14:35 +0100

    fixed outdoors folder creation

--- d_rats/config.py
+++ d_rats/config.py
@@ -1899,8 +1899,8 @@ class DratsConfig(six.moves.configparser.ConfigParser):
             os.mkdir(os.path.join(map_dir, "base"))
         if not os.path.exists(os.path.join(map_dir, "cycle")):
             os.mkdir(os.path.join(map_dir, "cycle"))
-        if not os.path.exists(os.path.join(map_dir, "outdoor")):
-            os.mkdir(os.path.join(map_dir, "outdoor"))
+        if not os.path.exists(os.path.join(map_dir, "outdoors")):
+            os.mkdir(os.path.join(map_dir, "outdoors"))
         if not os.path.exists(os.path.join(map_dir, "landscape")):
             os.mkdir(os.path.join(map_dir, "landscape"))
 
