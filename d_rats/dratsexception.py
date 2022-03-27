fb50870390c200cac0bfb97e55f4df52aeadc83d
John E. Malmberg <wb8tyw@qsl.net>
2022-03-27 08:17:56 -0500

    fix:(comm) Attempt to fix AGWPE (#129)
    I do not have an AGWPE driver, so this is the best that I can do
    using the existing code, the AGWPE developer documentation and a
    simple loopback test server for now.
    
    Based on what I found in testing, I do not think I made it any worse
    that what it was, but maybe it will help someone else with the
    actual AGWPE driver fix anything that I missed.
    
    d_rats/agw.py:
      Convert to logging module.
      Improve Docstrings.
      Fix a lot of pylint diagnostics.
      Converted to match current AGWPE documentation.
      Fix structures to use little-endian explicitly.
      Python3 compatible bytes usage.
      In AGWConnection.recv_frame method,
      fix to receive the entire frame instead of just the header.
      Add simple test server for simple sanity tests.
    
    d_rats/comm.py:
      Move exceptions into d_rats/dratsexeceptions.py for agw.py also to use.
      struct pack and unpack need to specify the proper-endian, which
      should be network-endian for new code, and little-endian only for
      backwards bug compatibility with most existing d-rats.
    
    d_rats/dratsexception.py: New file
      The agw.py need to be based on comm exceptions, but that
      means that the exceptions can not be defined in the comm.py
      module.
    
    d_rats/transport.py:
      Need to get the comm exceptions from d_rats/dratsexception.py
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

---
+++ d_rats/dratsexception.py
@@ -0,0 +1,32 @@
+'''Base d-rats Exception'''
+
+#
+# Copyright 2022 John Malmberg <wb8tyw@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# All exceptions unique to D-rats should be based on this class.
+# This class it self should not be ever raised.
+
+
+class DataPathError(Exception):
+    '''Data Path Error.'''
+
+
+class DataPathNotConnectedError(DataPathError):
+    '''Data Path Not Connected Error.'''
+
+
+class DataPathIOError(DataPathError):
+    '''Data Path IO Error'''
