bff9791f374b3806b1d1f8e9ca46da709b718038
John E. Malmberg <wb8tyw@qsl.net>
2022-03-27 14:44:24 -0500

    fix(setup): Pylint style fixes (#133)
    setup.py:
      First pass getting pylint issues resolved.
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-# Copyright 2008 Dan Smith <dsmith@danplanet.com> 
+'''Python Setup Module.'''
+# Copyright 2008 Dan Smith <dsmith@danplanet.com>
 # review 2015-2020 Maurizio Andreotti  <iz2lxi@yahoo.it>
+# Copyright 2022 John. E. Malmberg - Python3 Conversion
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,94 +18,137 @@
 
 from __future__ import absolute_import
 from __future__ import print_function
+
+import os
 import sys
 
 from d_rats.version import DRATS_VERSION
-import os
 
 def win32_build():
+    '''Microsoft Windows Build.'''
     from distutils.core import setup
-    import py2exe
+    # pylint: disable=import-error, unused-import
+    import py2exe # type: ignore
 
     try:
         # if this doesn't work, try import modulefinder
-        import py2exe.mf as modulefinder
-        import win32com
-        for p in win32com.__path__[1:]:
-            modulefinder.AddPackagePath("win32com", p)
+        import py2exe.mf as modulefinder # type: ignore
+        import win32com  # type: ignore
+        for path in win32com.__path__[1:]:
+            modulefinder.AddPackagePath("win32com", path)
         for extra in ["win32com.shell"]: #,"win32com.mapi"
             __import__(extra)
-            m = sys.modules[extra]
-            for p in m.__path__[1:]:
-                modulefinder.AddPackagePath(extra, p)
+            modules = sys.modules[extra]
+            for path in modules.__path__[1:]:
+                modulefinder.AddPackagePath(extra, path)
     except ImportError:
         # no build path setup, no worries.
         pass
 
-
     opts = {
         "py2exe" : {
-            "includes" : "pango,atk,gobject,cairo,pangocairo,win32gui,win32com,win32com.shell,email.iterators,email.generator,gio,simplejson,six,xmlrpclib,SimpleXMLRPCServer,SocketServer,BaseHTTPServer",
+            "includes" : "pango,atk,gobject,cairo,pangocairo" \
+                         ",win32gui,win32com,win32com.shell" \
+                         ",email.iterators,email.generator,gio" \
+                         ",simplejson,six,xmlrpclib" \
+                         ",SimpleXMLRPCServer,SocketServer" \
+                         ",BaseHTTPServer",
             'dll_excludes': [
-                              'API-MS-Win-Core-Debug-L1-1-0.dll' ,
-                              'API-MS-Win-Core-DelayLoad-L1-1-0.dll' ,
-                              'API-MS-Win-Core-ErrorHandling-L1-1-0.dll' ,
-                              'API-MS-Win-Core-File-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Handle-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Heap-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Interlocked-L1-1-0.dll' ,
-                              'API-MS-Win-Core-IO-L1-1-0.dll' ,
-                              'API-MS-Win-Core-LibraryLoader-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Localization-L1-1-0.dll' ,
-                              'API-MS-Win-Core-LocalRegistry-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Memory-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Misc-L1-1-0.dll' ,
-                              'API-MS-Win-Core-ProcessEnvironment-L1-1-0.dll' ,
-                              'API-MS-Win-Core-ProcessThreads-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Profile-L1-1-0.dll' ,
-                              'API-MS-Win-Core-String-L1-1-0.dll' ,
-                              'API-MS-Win-Core-Synch-L1-1-0.dll' ,
-                              'API-MS-Win-Core-SysInfo-L1-1-0.dll',
-                              'DNSAPI.DLL',
-                              'MSIMG32.DLL',
-                              'MSWSOCK.dll',
-                              'NSI.DLL' ,
-                              'USP10.DLL'
-                            ] ,
+                'API-MS-Win-Core-Debug-L1-1-0.dll',
+                'API-MS-Win-Core-DelayLoad-L1-1-0.dll',
+                'API-MS-Win-Core-ErrorHandling-L1-1-0.dll',
+                'API-MS-Win-Core-File-L1-1-0.dll',
+                'API-MS-Win-Core-Handle-L1-1-0.dll',
+                'API-MS-Win-Core-Heap-L1-1-0.dll',
+                'API-MS-Win-Core-Interlocked-L1-1-0.dll',
+                'API-MS-Win-Core-IO-L1-1-0.dll',
+                'API-MS-Win-Core-LibraryLoader-L1-1-0.dll',
+                'API-MS-Win-Core-Localization-L1-1-0.dll',
+                'API-MS-Win-Core-LocalRegistry-L1-1-0.dll',
+                'API-MS-Win-Core-Memory-L1-1-0.dll',
+                'API-MS-Win-Core-Misc-L1-1-0.dll',
+                'API-MS-Win-Core-ProcessEnvironment-L1-1-0.dll',
+                'API-MS-Win-Core-ProcessThreads-L1-1-0.dll',
+                'API-MS-Win-Core-Profile-L1-1-0.dll',
+                'API-MS-Win-Core-String-L1-1-0.dll',
+                'API-MS-Win-Core-Synch-L1-1-0.dll',
+                'API-MS-Win-Core-SysInfo-L1-1-0.dll',
+                'DNSAPI.DLL',
+                'MSIMG32.DLL',
+                'MSWSOCK.dll',
+                'NSI.DLL',
+                'USP10.DLL'
+                ],
             "compressed" : 1,
             "optimize" : 2,
             "bundle_files" : 3,
-           # "skip_archive" : True,
-            #        "packages" : ""
+            # "skip_archive" : True,
+            # "packages" : ""
             }
         }
 
 # Missing modules according to py3exe output
 #
-#'BeautifulSoup', 'FCNTL', 'Image', 'PIL', 'System', 'System.IO.Ports', 'TERMIOS', '_scproxy', '_speedups', '_sysconfigdata', 'clr', 'django.utils.simplejson', 'django.utils.six.moves.urllib.parse', 'dl', 'email.MIMEBase', 'email.MIMEMultipart', 'email.MIMEText', 'email.Message', 'feedparser', 'gdk', 'importlib.find_loader', 'importlib.reload', 'importlib.util', 'ossaudiodev', 'pytz', 'simplejson._speedups', 'unix', 'urllib.parse', 'glib.GError', 'glib.IOChannel', 'glib.IO_ERR', 'glib.IO_FLAG_APPEND', 'glib.IO_FLAG_GET_MASK', 'glib.IO_FLAG_IS_READABLE', 'glib.IO_FLAG_IS_SEEKABLE', 'glib.IO_FLAG_IS_WRITEABLE', 'glib.IO_FLAG_MASK', 'glib.IO_FLAG_NONBLOCK', 'glib.IO_FLAG_SET_MASK', 'glib.IO_HUP', 'glib.IO_IN', 'glib.IO_NVAL', 'glib.IO_OUT', 'glib.IO_PRI', 'glib.IO_STATUS_AGAIN', 'glib.IO_STATUS_EOF', 'glib.IO_STATUS_ERROR', 'glib.IO_STATUS_NORMAL', 'glib.Idle', 'glib.MainContext', 'glib.MainLoop', 'glib.OPTION_ERROR', 'glib.OPTION_ERROR_BAD_VALUE', 'glib.OPTION_ERROR_FAILED', 'glib.OPTION_ERROR_UNKNOWN_OPTION', 'glib.OPTION_FLAG_FILENAME', 'glib.OPTION_FLAG_HIDDEN', 'glib.OPTION_FLAG_IN_MAIN', 'glib.OPTION_FLAG_NOALIAS', 'glib.OPTION_FLAG_NO_ARG', 'glib.OPTION_FLAG_OPTIONAL_ARG', 'glib.OPTION_FLAG_REVERSE', 'glib.OPTION_REMAINING', 'glib.OptionContext', 'glib.OptionGroup', 'glib.PRIORITY_DEFAULT', 'glib.PRIORITY_DEFAULT_IDLE', 'glib.PRIORITY_HIGH', 'glib.PRIORITY_HIGH_IDLE', 'glib.PRIORITY_LOW', 'glib.Pid', 'glib.PollFD', 'glib.SPAWN_CHILD_INHERITS_STDIN', 'glib.SPAWN_DO_NOT_REAP_CHILD', 'glib.SPAWN_FILE_AND_ARGV_ZERO', 'glib.SPAWN_LEAVE_DESCRIPTORS_OPEN', 'glib.SPAWN_SEARCH_PATH', 'glib.SPAWN_STDERR_TO_DEV_NULL', 'glib.SPAWN_STDOUT_TO_DEV_NULL', 'glib.Source', 'glib.Timeout', 'glib.child_watch_add', 'glib.filename_display_basename', 'glib.filename_display_name', 'glib.filename_from_utf8', 'glib.get_application_name', 'glib.get_current_time', 'glib.get_prgname', 'glib.glib_version', 'glib.idle_add', 'glib.io_add_watch', 'glib.main_context_default', 'glib.main_depth', 'glib.markup_escape_text', 'glib.set_application_name', 'glib.set_prgname', 'glib.source_remove', 'glib.spawn_async', 'glib.timeout_add', 'glib.timeout_add_seconds', 'glib.uri_list_extract_uris', 'gtk.Assistant']
+# 'BeautifulSoup', 'FCNTL', 'Image', 'PIL', 'System', 'System.IO.Ports',
+# 'TERMIOS', '_scproxy', '_speedups', '_sysconfigdata', 'clr',
+# 'django.utils.simplejson', 'django.utils.six.moves.urllib.parse', 'dl',
+# 'email.MIMEBase', 'email.MIMEMultipart', 'email.MIMEText', 'email.Message',
+# 'feedparser', 'gdk', 'importlib.find_loader', 'importlib.reload',
+# 'importlib.util', 'ossaudiodev', 'pytz', 'simplejson._speedups', 'unix',
+# 'urllib.parse', 'glib.GError', 'glib.IOChannel', 'glib.IO_ERR',
+# 'glib.IO_FLAG_APPEND', 'glib.IO_FLAG_GET_MASK', 'glib.IO_FLAG_IS_READABLE',
+# 'glib.IO_FLAG_IS_SEEKABLE', 'glib.IO_FLAG_IS_WRITEABLE',
+# 'glib.IO_FLAG_MASK', 'glib.IO_FLAG_NONBLOCK', 'glib.IO_FLAG_SET_MASK',
+# 'glib.IO_HUP', 'glib.IO_IN', 'glib.IO_NVAL', 'glib.IO_OUT', 'glib.IO_PRI',
+# 'glib.IO_STATUS_AGAIN', 'glib.IO_STATUS_EOF', 'glib.IO_STATUS_ERROR',
+# 'glib.IO_STATUS_NORMAL', 'glib.Idle', 'glib.MainContext',
+# 'glib.MainLoop', 'glib.OPTION_ERROR', 'glib.OPTION_ERROR_BAD_VALUE',
+# 'glib.OPTION_ERROR_FAILED', 'glib.OPTION_ERROR_UNKNOWN_OPTION',
+# 'glib.OPTION_FLAG_FILENAME', 'glib.OPTION_FLAG_HIDDEN',
+# 'glib.OPTION_FLAG_IN_MAIN', 'glib.OPTION_FLAG_NOALIAS',
+# 'glib.OPTION_FLAG_NO_ARG', 'glib.OPTION_FLAG_OPTIONAL_ARG',
+# 'glib.OPTION_FLAG_REVERSE', 'glib.OPTION_REMAINING', 'glib.OptionContext',
+# 'glib.OptionGroup', 'glib.PRIORITY_DEFAULT', 'glib.PRIORITY_DEFAULT_IDLE',
+# 'glib.PRIORITY_HIGH', 'glib.PRIORITY_HIGH_IDLE', 'glib.PRIORITY_LOW',
+# 'glib.Pid', 'glib.PollFD', 'glib.SPAWN_CHILD_INHERITS_STDIN',
+# 'glib.SPAWN_DO_NOT_REAP_CHILD', 'glib.SPAWN_FILE_AND_ARGV_ZERO',
+# 'glib.SPAWN_LEAVE_DESCRIPTORS_OPEN', 'glib.SPAWN_SEARCH_PATH',
+# 'glib.SPAWN_STDERR_TO_DEV_NULL', 'glib.SPAWN_STDOUT_TO_DEV_NULL',
+# 'glib.Source', 'glib.Timeout', 'glib.child_watch_add',
+# 'glib.filename_display_basename', 'glib.filename_display_name',
+# 'glib.filename_from_utf8', 'glib.get_application_name',
+# 'glib.get_current_time', 'glib.get_prgname', 'glib.glib_version',
+# 'glib.idle_add', 'glib.io_add_watch', 'glib.main_context_default',
+# 'glib.main_depth', 'glib.markup_escape_text', 'glib.set_application_name',
+# 'glib.set_prgname', 'glib.source_remove', 'glib.spawn_async',
+# 'glib.timeout_add', 'glib.timeout_add_seconds',
+# 'glib.uri_list_extract_uris', 'gtk.Assistant']
 #
-
-
     setup(
         windows=[{'script' : "d-rats.py",
                   'icon_resources': [(0x0004, 'd-rats2.ico')]},
                  {'script' : 'd-rats_repeater.py'}],
-# adapt the following line for your system
+        # adapt the following line for your system
         data_files=["C:\\GTK\\bin\\jpeg62.dll"],
         options=opts)
 
 def macos_build():
+    '''Mac OS Build.'''
     from setuptools import setup
     import shutil
 
+    # pylint: disable=invalid-name
     APP = ['d-rats-%s.py' % DRATS_VERSION]
     shutil.copy("d-rats", APP[0])
+    # pylint: disable=invalid-name
     DATA_FILES = [('../Frameworks',
                    ['/opt/local/lib/libpangox-1.0.0.2203.1.dylib']),
-                  ('../Resources/pango/1.6.0/modules', ['/opt/local/lib/pango/1.6.0/modules/pango-basic-atsui.so']),
+                  ('../Resources/pango/1.6.0/modules',
+                   ['/opt/local/lib/pango/1.6.0/modules/pango-basic-atsui.so']),
                   ('../Resources',
                    ['images', 'ui']),
                   ]
+    # pylint: disable=invalid-name
     OPTIONS = {'argv_emulation': True, "includes" : "gtk,atk,pangocairo,cairo"}
 
     setup(
@@ -114,6 +159,7 @@ def macos_build():
         )
 
 def default_build():
+    '''Default Build.'''
     from distutils.core import setup
     from glob import glob
 
@@ -129,13 +175,14 @@ def default_build():
     os.system("make -C libexec")
 
     man_files = []
-    for f in _man_files:
-        os.system("gzip -c %s > %s" % (f, f+".gz"))
-        man_files.append(f+".gz")
+    for fname in _man_files:
+        os.system("gzip -c %s > %s" % (fname, fname + ".gz"))
+        man_files.append(fname + ".gz")
 
     locale_files = []
-    for f in _locale_files:
-        locale_files.append(("/usr/share/d-rats/%s" % os.path.dirname(f), [f]))
+    for fname in _locale_files:
+        locale_files.append(("/usr/share/d-rats/%s" % os.path.dirname(fname),
+                             [fname]))
 
     print(("LOC: %s" % str(ui_files)))
 
@@ -157,12 +204,10 @@ def default_build():
                     ('/usr/share/man/man1', man_files),
                     ('/usr/share/doc/d-rats', ['COPYING']),
                     ] + locale_files)
-                    
+
 if sys.platform == "darwin":
     macos_build()
 elif sys.platform == "win32":
     win32_build()
 else:
     default_build()
-
-
