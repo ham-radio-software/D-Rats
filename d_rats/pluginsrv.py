ad7910fcaa37dc96cb53adb5eb2f35fcd8ade349
John E. Malmberg <wb8tyw@qsl.net>
2022-04-19 07:31:26 -0500

    fix(pluginsrv): Python3 Conversion (#163)
    d_rats/pluginsrv.py:
      Remove python 2 compatibility.
      Restore use of fixed port.
      Improve Docstrings.
      Raise named exceptions.
      Convert to logging module.
      Replace "insecure" eval call.
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

--- d_rats/pluginsrv.py
+++ d_rats/pluginsrv.py
@@ -2,6 +2,7 @@
 '''Pluginsrv'''
 #
 # Copyright 2009 Dan Smith <dsmith@danplanet.com>
+# Copyright 2021-2022 John. E. Malmberg - Python3 Conversion
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,9 +20,11 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
+import ast
+import logging
 import os
 import threading
-from six.moves.xmlrpc_server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCServer
 
 import gi
 gi.require_version("Gtk", "3.0")
@@ -29,14 +32,27 @@ from gi.repository import GObject
 
 from . import signals
 from . import utils
-# from d_rats.sessions import rpc
 
-# importing printlog() wrapper
-from .debug import printlog
+
+class PluginSrvException(Exception):
+    '''Generic PluginSrv Exception.'''
+
+
+class ProxyStationNotHeard(PluginSrvException):
+    '''Station Not Heard Exception.'''
+
+
+class ProxyInvalidRPCName(PluginSrvException):
+    '''Invalid RPC Name Exception.'''
 
 
 class DRatsChatEvent():
-    '''DRats Chat Event.'''
+    '''
+    DRats Chat Event.
+
+    :param src_station: Source station, default None
+    :type src_station: str
+    '''
 
     def __init__(self, src_station=None):
         self.__event = threading.Event()
@@ -48,6 +64,7 @@ class DRatsChatEvent():
         Get source station.
 
         :returns: Source station
+        :rtype: str
         '''
         return self.__src_station
 
@@ -55,8 +72,10 @@ class DRatsChatEvent():
         '''
         Set Chat Info
 
-        :param src: Source for chat event
+        :param src: Source Station for chat event
+        :type src: str
         :param text: Text for chat event
+        :type text: str
         '''
         self.__src_station = src
         self.__text = text
@@ -66,18 +85,20 @@ class DRatsChatEvent():
         Get Text.
 
         :returns: Event text
+        :rtype: str
         '''
         return self.__text
 
     def set(self):
-        '''set event.'''
+        '''Set Event.'''
         self.__event.set()
 
     def wait(self, timeout):
         '''
         Wait for a timeout.
 
-        :param timeout: Timeout to wait in unknown units
+        :param timeout: Timeout to wait in seconds
+        :type timeout: float
         '''
         self.__event.wait(timeout)
 
@@ -95,6 +116,7 @@ class DRatsPluginProxy(GObject.GObject):
 
     def __init__(self):
         GObject.GObject.__init__(self)
+        self.logger = logging.getLogger("DRatsPluginProxy")
         self.__idcount = 0
         self.__persist = {}
 
@@ -107,15 +129,15 @@ class DRatsPluginProxy(GObject.GObject):
         Get port for station.
 
         :param station: Station to get the port for
-        :raises Exception: when port is not known
+        :type station: str
         :returns: port
+        :rtype: str
+        :raises: :class:`ProxyStationNotHeard` when port is not known
         '''
         ports = self.emit("get-station-list")
         port = utils.port_for_station(ports, station)
         if not port:
-            # Need to define this as a specific exception
-            raise Exception("Station %s not heard" % station)
-
+            raise ProxyStationNotHeard("Station %s not heard" % station)
         return port
 
     def send_chat(self, port, message):
@@ -123,11 +145,13 @@ class DRatsPluginProxy(GObject.GObject):
         Send a chat message on port
 
         :param port: Port to send message on
+        :type port: str
         :param message: Message to send
+        :type message: str
         :returns: 0
+        :rtype: int
         '''
-        printlog("Pluginsrv",
-                 " : Sending chat on port %s: %s" % (port, message))
+        self.logger.info("send_chat: port %s: %s", port, message)
         self.emit("user-send-chat", "CQCQCQ", port, message, False)
 
         return 0
@@ -137,6 +161,7 @@ class DRatsPluginProxy(GObject.GObject):
         List of port names.
 
         :returns: List of port names
+        :rtype: list[str]
         '''
         slist = self.emit("get-station-list")
         return list(slist.keys())
@@ -146,24 +171,26 @@ class DRatsPluginProxy(GObject.GObject):
         Send a file to station specified by filename on optional port.
 
         If port is not specified, the last-heard port for station will be
-        used.  An exception will be thrown if the last port cannot be
-        determined.
+        used.
 
         :param station: Destination.
+        :type station: str
         :param filename: File to send.
+        :type filename: str
         :param port: Optional port to use.
-        :raises: Unspecified exception if last port can not be determined.
+        :type port: str
         :returns: 0
+        :rtype: int
+        :raises: :class:`ProxyStationNotHeard` if last port can not be
+                 determined.
         '''
-        # Need to find out exception raised and documented it.
         if not port:
             port = self.get_port(station)
 
         sname = os.path.basename(filename)
 
-        printlog("Pluginsrv",
-                 " : Sending file %s to %s on port %s" %
-                 (filename, station, port))
+        self.logger.info("send_file: %s to %s on port %s",
+                         filename, station, port)
         self.emit("user-send-file", station, port, filename, sname)
 
         return 0
@@ -177,19 +204,25 @@ class DRatsPluginProxy(GObject.GObject):
         items to call on the job object before submission.
 
         :param station: Destination Station
+        :type station: str
         :param rpcname: Name of the rpcname
+        :type rpcname: str
         :param port: Optional port.
+        :type port: str
         :param params: Optional parameters for RPC job
-        :returns: Job specifier to be used with get_result()
+        :type params: dict
+        :returns: Job identification number
+        :rtype: int
+        :raises: :class:`ProxyInvalidRPCName` If an invalid RPC name used
         '''
         if not rpcname.isalpha() or not rpcname.startswith("RPC"):
-            raise Exception("Invalid RPC function call name")
+            raise ProxyInvalidRPCName("Invalid RPC function call name")
 
         if not port:
             port = self.get_port(station)
 
-        # pylint: disable=eval-used
-        job = eval("rpcsession.%s('%s', 'New Job')" % (rpcname, station))
+        job = ast.literal_eval("rpcsession.%s('%s', 'New Job')" %
+                               (rpcname, station))
         if params:
             for key, val in params:
                 func = job.__getattribute__(key)
@@ -211,7 +244,9 @@ class DRatsPluginProxy(GObject.GObject):
         Get the result of job ident.
 
         :param ident: Ident of job.
-        :returns: Structure, empty until completion
+        :type ident: int
+        :returns: Job result if in persist state
+        :rtype: dict
         '''
         if ident in self.__persist:
             result = self.__persist[ident]
@@ -229,8 +264,11 @@ class DRatsPluginProxy(GObject.GObject):
         from that station is received.
 
         :param timeout: Time out to wait
+        :type timeout: float
         :param src_station: Optional Station to wait for message from
-        :returns: Two element tuple of source station and chat text
+        :type src_station:str
+        :returns: Source station and chat text
+        :rtype: tuple[str, str]
         '''
         event = DRatsChatEvent(src_station)
         self.__events["chat"].append(event)
@@ -238,16 +276,18 @@ class DRatsPluginProxy(GObject.GObject):
 
         if event.get_text():
             return event.get_src_station(), event.get_text()
-        else:
-            return "", ""
+        return "", ""
 
     def incoming_chat_message(self, src, _dst, text):
         '''
         Incoming Chat Message.
 
         :param src: Source of message
-        :param dst: Destination of message
+        :type src: str
+        :param _dst: Destination of message, Unused
+        :type _dst: str
         :param text: Text of message
+        :type text: str
         '''
         for event in self.__events["chat"]:
             if not event.get_src_station():
@@ -263,8 +303,8 @@ class DRatsPluginServer(SimpleXMLRPCServer):
 
     def __init__(self):
 
-        SimpleXMLRPCServer.__init__(self, ("localhost", 0))
-
+        SimpleXMLRPCServer.__init__(self, ("localhost", 9100))
+        self.logger = logging.getLogger("DRatsPluginServer")
         self.__thread = None
 
         self.__proxy = DRatsPluginProxy()
@@ -281,7 +321,7 @@ class DRatsPluginServer(SimpleXMLRPCServer):
         self.__thread = threading.Thread(target=self.serve_forever)
         self.__thread.setDaemon(True)
         self.__thread.start()
-        printlog("Pluginsrv", " : Started serve_forever() thread")
+        self.logger.info("Started serve_forever() thread")
 
     def incoming_chat_message(self, *args):
         '''
