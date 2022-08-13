568e686e609e800d55589cb5503fc6f3447dd849
John E. Malmberg <wb8tyw@qsl.net>
2022-08-13 10:58:19 -0500

    fix(tests) Add pre-commit hook (#252)
    Add a pre-commit hook and a pylint check.
    This is to catch errors that were missed in testing before
    you commit a change.
    
    More checks will be added in the future.
    
    Use 'cp tests/pre-commit .git/hooks' to activate.
    Make sure that you have execute accss to the file, on Linux
    that should already be the case.
    
    tests/pre-commit: (New file)
      A pre-commit hook to be copied into .git/hooks
    
    tests/pylint_check.sh:
      A pylint checking script
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

---
+++ tests/pre-commit_d/pylint_check.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -ue
+
+# Default list of files to lint
+: "${CHANGED_FILES:=}"
+if [ -z "$CHANGED_FILES" ]; then
+  # Nothing to do
+  exit 0
+fi
+
+# Default output
+: "${PYLINT_OUT:=pylint.log}"
+# Extra Pylint Options
+: "${PYLINT_OPTS:=}"
+# Default directory to start check
+: "${BASE_DIR:=.}"
+
+# Default checking
+# pylint_rc="$(find "${BASE_DIR}" -name pylint.rc -print -quit)"
+
+# Now search for all python files
+rc=0
+pushd "${BASE_DIR}" > /dev/null || exit 1
+
+  rm -f "${PYLINT_OUT}"
+
+  # Template to allow other tools to parse the output
+  tmpl="{path}:{line}: pylint-{symbol}: {msg}"
+
+  pylint="$(command -v pylint)"
+  if [ -z "${pylint}" ]; then
+    echo "pylint not found"
+    exit 1
+  fi
+
+  for script_file in ${CHANGED_FILES}; do
+
+    IFS=" " read -r -a pylint_cmd <<< "${pylint} ${PYLINT_OPTS} ${script_file}"
+    if file "$script_file" | grep -q -e 'Python script'; then
+      if ! python "${pylint_cmd[@]}" --msg-template "${tmpl}" >> \
+        "${PYLINT_OUT}" 2>&1; then
+        (( rc=rc+PIPESTATUS[0] ))
+      fi
+    fi
+  done
+  if [ -e "${PYLINT_OUT}" ]; then
+    grep ':' "${PYLINT_OUT}"
+  fi
+popd > /dev/null || exit 1
+exit "${rc}"
