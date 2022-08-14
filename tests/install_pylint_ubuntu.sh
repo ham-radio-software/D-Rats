86b051006bf6d7c14a62ffb85d6eeb1ebaed91ef
John E. Malmberg <wb8tyw@qsl.net>
2022-08-14 11:19:10 -0500

    test(pylint) Add pylint github action (#253)
    .github/workflows/pylint.yml:
      Pylint action for github
    
    tests/install_pylint_ubuntu.sh:
      Script to make sure pylint is installed on Ubuntu
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

---
+++ tests/install_pylint_ubuntu.sh
@@ -0,0 +1,23 @@
+#/bin/bash
+
+# This is for use in a dockerfile or a github action to make sure
+# that the required packages are installed.
+
+set -uex
+
+# Need to disable are-you-sure prompts during the installs
+DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND
+# echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/no-prompt
+# echo "APT::Install-Recommends \"false\";" > \
+#    /etc/apt/apt.conf.d/no-recommends
+
+# Update to current and add packages
+sudo -S -E apt-get --assume-yes update
+sudo -S -E apt-get --assume-yes install aptitude
+sudo -S -E aptitude --assume-yes safe-upgrade
+sudo -S -E apt-get --assume-yes install \
+    git \
+    grep \
+    pylint \
+    python3
