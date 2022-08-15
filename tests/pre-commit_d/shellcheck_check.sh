fcf6bd5b9a2e1469bddcffb9352821feab854fa8
John E. Malmberg <wb8tyw@qsl.net>
2022-08-15 07:43:36 -0500

    test(shellcheck): Add shellcheck to linting (#254)
    .github/workflows/shellcheck.yml: (new)
      Start a workflow to check shell scripts.
    
    tests/install_pylint_ubuntu.sh:
      Missing install of file package.
    
    tests/install_shellcheck_ubuntu.sh: (new)
      Make sure shellcheck is installed for github action.
    
    tests/pre-commit_d/shellcheck_check.sh: (new)
      Shellcheck check for pre-commit and github action.
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

---
+++ tests/pre-commit_d/shellcheck_check.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ue
+
+# Default list of files to lint
+: "${CHANGED_FILES:=}"
+if [ -z "$CHANGED_FILES" ]; then
+  # Nothing to do
+  exit 0
+fi
+
+# Default output
+: "${SHELLCHECK_OUT:=shellcheck.log}"
+# Default directory to start check
+: "${BASE_DIR:=.}"
+
+# Now search for all shell script files
+rc=0
+pushd "${BASE_DIR}" > /dev/null || exit 1
+
+  rm -f "${SHELLCHECK_OUT}"
+
+  # Template to allow other tools to parse the output
+  tmpl="--format=gcc"
+
+  shellcheck="$(command -v shellcheck)"
+  if [ -z "${shellcheck}" ]; then
+    echo "shellcheck not found"
+    exit 1
+  fi
+
+  for script_file in ${CHANGED_FILES}; do
+
+    IFS=" " read -r -a shellcheck_cmd <<< "${shellcheck} ${script_file}"
+    if file "$script_file" | grep -q -e 'shell script'; then
+      if ! "${shellcheck_cmd[@]}" "${tmpl}" >> "${SHELLCHECK_OUT}" 2>&1; then
+        (( rc=rc+PIPESTATUS[0] ))
+      fi
+    fi
+  done
+  if [ "$rc" -ne 0 ] && [ -e "${SHELLCHECK_OUT}" ]; then
+    grep ':' "${SHELLCHECK_OUT}"
+  fi
+popd > /dev/null || exit 1
+exit "${rc}"
