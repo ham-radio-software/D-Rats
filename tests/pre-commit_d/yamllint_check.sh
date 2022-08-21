50d65d2f982a8012f56deb236945b73fd04c22a6
John E. Malmberg <wb8tyw@qsl.net>
2022-08-21 11:49:57 -0500

    fix(yamllint) Add yamllint checking (#263)
    .github/workflows/yamllint.yml: (new)
      Github action for checking yaml.
    
    tests/pre-commit_d/yamllint_check.sh: (new)
      Git hook to checking yaml.
    
    .github/workflows/codespell.yml:
    .github/workflows/pylint.yml:
    .github/workflows/shellcheck.yml:
    .github/workflows/xmllint.yml:
      Fixed to pass yamllint scan.
    
    README.md:
      add yamllint to list of development packages.
    
    Co-authored-by: John E. Malmberg <wb8tyw@gmail.com>

---
+++ tests/pre-commit_d/yamllint_check.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ue
+
+# Default list of files to lint
+: "${CHANGED_FILES:=}"
+if [ -z "$CHANGED_FILES" ]; then
+  # Nothing to do
+  exit 0
+fi
+
+# Default output
+: "${YAMLLINT_OUT:=yamllint.log}"
+# Default directory to start check
+: "${BASE_DIR:=.}"
+
+# Now search for all shell script files
+rc=0
+pushd "${BASE_DIR}" > /dev/null || exit 1
+
+  rm -f "${YAMLLINT_OUT}"
+
+  yamllint="$(command -v yamllint)"
+  if [ -z "${yamllint}" ]; then
+    echo "yamllint not found"
+    exit 0
+  fi
+
+  for script_file in ${CHANGED_FILES}; do
+
+    IFS=" " read -r -a yamllint_cmd <<< \
+        "${yamllint} --strict --format github ${script_file}"
+    if [[ "$script_file" == *.yml ]]; then
+      if ! "${yamllint_cmd[@]}" >> "${YAMLLINT_OUT}" 2>&1; then
+        (( rc=rc+PIPESTATUS[0] ))
+      fi
+    fi
+  done
+  if [ "$rc" -ne 0 ] && [ -e "${YAMLLINT_OUT}" ]; then
+    if ! grep '::' "${YAMLLINT_OUT}"; then
+      cat "${YAMLLINT_OUT}"
+    fi
+  fi
+popd > /dev/null || exit 1
+exit "${rc}"
